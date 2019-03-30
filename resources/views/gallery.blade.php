b039730139721a65de1d3db18f012b79235b9f1b
Ifeanyi Osinakayah Joseph <ifeanyiosinakayah15@gmail.com>
2019-03-31 00:36:14 +0100

    First commit

---
+++ resources/views/gallery.blade.php
@@ -0,0 +1,364 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Hello World</title>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- FontAwesome CSS -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <!-- ElegantFonts CSS -->
+    <link rel="stylesheet" href="css/elegant-fonts.css">
+
+    <!-- themify-icons CSS -->
+    <link rel="stylesheet" href="css/themify-icons.css">
+
+    <!-- Swiper CSS -->
+    <link rel="stylesheet" href="css/swiper.min.css">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="single-page portfolio">
+<header class="site-header">
+    <div class="top-header-bar">
+        <div class="container">
+            <div class="row flex-wrap justify-content-center justify-content-lg-between align-items-lg-center">
+                <div class="col-12 col-lg-8 d-none d-md-flex flex-wrap justify-content-center justify-content-lg-start mb-3 mb-lg-0">
+                    <div class="header-bar-email">
+                        MAIL: <a href="#">mariecareinitiative@gmail.com</a>
+                    </div><!-- .header-bar-email -->
+
+                    <div class="header-bar-text">
+                        <p>PHONE: <span>+2348032633071</span></p>
+                    </div><!-- .header-bar-text -->
+                </div><!-- .col -->
+
+                <div class="col-12 col-lg-4 d-flex flex-wrap justify-content-center justify-content-lg-end align-items-center">
+                    <div class="donate-btn">
+                        <a href="#">Donate Now</a>
+                    </div><!-- .donate-btn -->
+                </div><!-- .col -->
+            </div><!-- .row -->
+        </div><!-- .container -->
+    </div><!-- .top-header-bar -->
+
+    <div class="nav-bar">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex flex-wrap justify-content-between align-items-center">
+                    <div class="site-branding d-flex align-items-center">
+                        <a class="d-block" href="/" rel="home"><img style="height: auto;width: 3rem;" class="d-block" src="images/logo.jpeg" alt="logo"></a>
+                    </div><!-- .site-branding -->
+
+                    <nav class="site-navigation d-flex justify-content-end align-items-center">
+                        <ul class="d-flex flex-column flex-lg-row justify-content-lg-end align-content-center">
+                            <li ><a href="/">Home</a></li>
+                            <li><a href="about">About us</a></li>
+                            <li class="current-menu-item"><a href="portfolio">Gallery</a></li>
+                            <li><a href="contact">Contact</a></li>
+                        </ul>
+                    </nav><!-- .site-navigation -->
+
+                    <div class="hamburger-menu d-lg-none">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div><!-- .hamburger-menu -->
+                </div><!-- .col -->
+            </div><!-- .row -->
+        </div><!-- .container -->
+    </div><!-- .nav-bar -->
+</header><!-- .site-header -->
+
+<div class="page-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Portfolio</h1>
+            </div><!-- .col -->
+        </div><!-- .row -->
+    </div><!-- .container -->
+</div><!-- .page-header -->
+
+<div class="portfolio-wrap">
+    <div class="container">
+        <div class="row portfolio-container">
+            <div class="col-12 col-md-6 col-lg-4 portfolio-item">
+                <div class="portfolio-cont">
+                    <a href="#"><img src="images/a.jpg" alt=""></a>
+
+                    <!--<h3 class="entry-title"><a href="#">Toys for Children Campaign</a></h3>-->
+                    <!--<h4>2018 Causes</h4>-->
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-3 portfolio-item">
+                <div class="portfolio-cont">
+                    <a href="#"><img src="images/b.jpg" alt=""></a>
+
+                    <!--<h3 class="entry-title"><a href="#">Toys for Children Campaign</a></h3>-->
+                    <!--<h4>2018 Causes</h4>-->
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-5 portfolio-item">
+                <div class="portfolio-cont">
+                    <a href="#"><img src="images/c.jpg" alt=""></a>
+
+                    <!--<h3 class="entry-title"><a href="#">Water for Children AID</a></h3>-->
+                    <!--<h4>2018 Causes</h4>-->
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-5 portfolio-item">
+                <div class="portfolio-cont">
+                    <a href="#"><img src="images/d.jpg" alt=""></a>
+
+                    <!--<h3 class="entry-title"><a href="#">Water for Children AID</a></h3>-->
+                    <!--<h4>2018 Causes</h4>-->
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-3 mt-48 portfolio-item">
+                <div class="portfolio-cont">
+                    <a href="#"><img src="images/e.jpg" alt=""></a>
+
+                    <!--<h3 class="entry-title"><a href="#">Toys for Children Campaign</a></h3>-->
+                    <!--<h4>2018 Causes</h4>-->
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4 mt-48 portfolio-item">
+                <div class="portfolio-cont">
+                    <a href="#"><img src="images/f.jpg" alt=""></a>
+
+                    <!--<h3 class="entry-title"><a href="#">Toys for Children Campaign</a></h3>-->
+                    <!--<h4>2018 Causes</h4>-->
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-3 mt-72 portfolio-item">
+                <div class="portfolio-cont">
+                    <a href="#"><img src="images/g.jpg" alt=""></a>
+
+                    <!--<h3 class="entry-title"><a href="#">Toys for Children Campaign</a></h3>-->
+                    <!--<h4>2018 Causes</h4>-->
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-6 mt-72 portfolio-item">
+                <div class="portfolio-cont">
+                    <a href="#"><img src="images/h.jpg" alt=""></a>
+
+                    <!--<h3 class="entry-title"><a href="#">Water for Children AI</a>D</h3>-->
+                    <!--<h4>2018 Causes</h4>-->
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-3 mt-72 portfolio-item">
+                <div class="portfolio-cont">
+                    <a href="#"><img src="images/i.jpg" alt=""></a>
+
+                    <!--<h3 class="entry-title"><a href="#">Toys for Children Campaign</a></h3>-->
+                    <!--<h4>2018 Causes</h4>-->
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4 portfolio-item">
+                <div class="portfolio-cont">
+                    <a href="#"><img src="images/a.jpg" alt=""></a>
+
+                    <!--<h3 class="entry-title"><a href="#">Toys for Children Campaign</a></h3>-->
+                    <!--<h4>2018 Causes</h4>-->
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-3 portfolio-item">
+                <div class="portfolio-cont">
+                    <a href="#"><img src="images/b.jpg" alt=""></a>
+
+                    <!--<h3 class="entry-title"><a href="#">Toys for Children Campaign</a></h3>-->
+                    <!--<h4>2018 Causes</h4>-->
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-5 portfolio-item">
+                <div class="portfolio-cont">
+                    <a href="#"><img src="images/c.jpg" alt=""></a>
+
+                    <!--<h3 class="entry-title"><a href="#">Water for Children AID</a></h3>-->
+                    <!--<h4>2018 Causes</h4>-->
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-5 portfolio-item">
+                <div class="portfolio-cont">
+                    <a href="#"><img src="images/d.jpg" alt=""></a>
+
+                    <!--<h3 class="entry-title"><a href="#">Water for Children AID</a></h3>-->
+                    <!--<h4>2018 Causes</h4>-->
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-3 mt-48 portfolio-item">
+                <div class="portfolio-cont">
+                    <a href="#"><img src="images/e.jpg" alt=""></a>
+
+                    <!--<h3 class="entry-title"><a href="#">Toys for Children Campaign</a></h3>-->
+                    <!--<h4>2018 Causes</h4>-->
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4 mt-48 portfolio-item">
+                <div class="portfolio-cont">
+                    <a href="#"><img src="images/f.jpg" alt=""></a>
+
+                    <!--<h3 class="entry-title"><a href="#">Toys for Children Campaign</a></h3>-->
+                    <!--<h4>2018 Causes</h4>-->
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-3 mt-72 portfolio-item">
+                <div class="portfolio-cont">
+                    <a href="#"><img src="images/g.jpg" alt=""></a>
+
+                    <!--<h3 class="entry-title"><a href="#">Toys for Children Campaign</a></h3>-->
+                    <!--<h4>2018 Causes</h4>-->
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-6 mt-72 portfolio-item">
+                <div class="portfolio-cont">
+                    <a href="#"><img src="images/h.jpg" alt=""></a>
+
+                    <!--<h3 class="entry-title"><a href="#">Water for Children AI</a>D</h3>-->
+                    <!--<h4>2018 Causes</h4>-->
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-3 mt-72 portfolio-item">
+                <div class="portfolio-cont">
+                    <a href="#"><img src="images/i.jpg" alt=""></a>
+
+                    <!--<h3 class="entry-title"><a href="#">Toys for Children Campaign</a></h3>-->
+                    <!--<h4>2018 Causes</h4>-->
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<footer class="site-footer">
+    <div class="footer-widgets">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="foot-about">
+                        <h2><a class="foot-logo" href="#"><img src="images/logo.jpeg" style="width: 3em" alt=""></a></h2>
+
+                        <p>MARIECARE INITIATIVE is a non-profit organization (NGO), founded in 2017 with the common aspiration of meeting the essential needs of the underprivileged women and children in our society.</p>
+
+                        <ul class="d-flex flex-wrap align-items-center">
+                            <li><a href="http://www.facebook.com/mariecareinitiative"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="http://www.twitter.com/mariecareinitiative"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="https://www.instagram.com/mariecareinitiative/"><i class="fa fa-instagram"></i></a></li>
+                        </ul>
+                    </div><!-- .foot-about -->
+                </div><!-- .col -->
+
+                <div class="col-12 col-md-6 col-lg-4 mt-5 mt-md-0">
+                    <h2>Useful Links</h2>
+
+                    <ul>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Become  a Volunteer</a></li>
+                        <li><a href="#">Donate</a></li>
+                    </ul>
+                </div><!-- .col -->
+
+                <div class="col-12 col-md-6 col-lg-4 mt-5 mt-md-0">
+                    <div class="foot-contact">
+                        <h2>Contact</h2>
+
+                        <ul>
+                            <li><i class="fa fa-phone"></i><span>+2348032599864</span></li>
+                            <li><i class="fa fa-envelope"></i><span>mariecareinitiative@gmail.com</span></li>
+                            <li><i class="fa fa-map-marker"></i><span>N0.2 Living Spring Chapel Avenue, Opp. Muhammadu Buhari international Market New Karu</span></li>
+                        </ul>
+                    </div><!-- .foot-contact -->
+
+                    <div class="subscribe-form">
+                        <form class="d-flex flex-wrap align-items-center">
+                            <input type="email" placeholder="Your email">
+                            <input type="submit" value="send">
+                        </form><!-- .flex -->
+                    </div><!-- .search-widget -->
+                </div><!-- .col -->
+            </div><!-- .row -->
+        </div><!-- .container -->
+    </div><!-- .footer-widgets -->
+
+    <div class="footer-bar">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <p class="m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                </div><!-- .col-12 -->
+            </div><!-- .row -->
+        </div><!-- .container -->
+    </div><!-- .footer-bar -->
+</footer><!-- .site-footer -->
+
+<script type='text/javascript' src='js/jquery.js'></script>
+<script type='text/javascript' src='js/jquery.collapsible.min.js'></script>
+<script type='text/javascript' src='js/swiper.min.js'></script>
+<script type='text/javascript' src='js/jquery.countdown.min.js'></script>
+<script type='text/javascript' src='js/circle-progress.min.js'></script>
+<script type='text/javascript' src='js/jquery.countTo.min.js'></script>
+<script type='text/javascript' src='js/jquery.barfiller.js'></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script type='text/javascript' src='js/custom.js'></script>
+
+<div class="modal" id="donateModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 class="modal-title">Make a Donation</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <!-- Modal body -->
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <label for="amount">Amount</label>
+                        <input type="number" class="form-control" id="amount">
+                    </div>
+                    <button type="submit" class="btn btn-primary btn gradient-bg">Donate</button>
+                </form>
+            </div>
+
+            <!-- Modal footer -->
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
